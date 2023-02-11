2591ae715693f22b29b827fb0a4bc0eee87ad7c2
Vincent <develop@unipayment.io>
2023-02-11 16:57:03 +0800

    fix name field

--- voip/models/ip_black_list.py
+++ voip/models/ip_black_list.py
@@ -8,6 +8,6 @@ class IP(models.Model):
     """
     IP in black list
     """
-    name = models.CharField('Name', max_length=100, default='Node', unique=True)
+    name = models.CharField('Name', max_length=100, default='Node')
     ip = models.CharField('IP', max_length=50, default='127.0.0.1')
     is_blocked = models.BooleanField('Blocked', default=False)
